2a0ef9c90e403aeab929268924b2689fe2879e20
Mahesh <mahesh.patil388@gmail.com>
2025-01-26 15:39:11 +0530

    changes done in main

--- one.py
+++ one.py
@@ -1,3 +1,14 @@
-numbers=range(1,11)
-res=list(filter(lambda x:x%2,numbers))
-print(res)
+# numbers=range(1,11)
+# res=list(filter(lambda x:x%2,numbers))
+# print(res),
+
+d="Maheshshhsh"
+new={}
+for x in d:
+    if x not in new.keys():
+        new[x]=1
+    else:
+        new[x]+=1
+print(new)
+for k,v in new.items():
+    print(f"{k} occured in {v} times")
